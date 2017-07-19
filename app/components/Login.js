9855e76700ac11e5157406df6bce134a9a4a9847
Graham Lutz <graham@brainbytescreative.com>
2017-07-19 09:04:22 -0400

    creates basic Login component

---
+++ app/components/Login.js
@@ -0,0 +1,49 @@
+
+const React = require('react')
+const ReactRouter = require('react-router');
+const Link = ReactRouter.Link;
+
+class Login extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+
+		this.handleNameChange     = this.handleNameChange.bind(this)
+		this.handleEmailChange    = this.handleEmailChange.bind(this)
+		this.handlePasswordChange = this.handlePasswordChange.bind(this)
+	}
+
+	handleNameChange(e) {
+
+	}
+	handleEmailChange(e) {
+		
+	}
+	handlePasswordChange(e) {
+		
+	}
+	render() {
+		return (
+			<div>
+			    <h2>Login</h2>
+			    <form onSubmit={this.handleSubmit}>
+			    	<label>
+			    		Name:
+			    		<input type="text" /*value={this.state.value}*/ onChange={this.handleNameChange}/>
+			    	</label>
+			    	<label>
+			    		Email:
+			    		<input type="text" /*value={this.state.value}*/ onChange={this.handleEmailChange}/>
+			    	</label>
+			    	<label>
+			    		Password:
+			    		<input type="text" /*value={this.state.value}*/ onChange={this.handlePasswordChange}/>
+			    	</label>
+			    	<input type="submit" value="Submit" />
+			    </form>
+			</div>
+		)
+	}
+}
+
+module.exports = Login;
